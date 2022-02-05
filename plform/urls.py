121e919c2bcb9c4740ddae7e676e5a85a2ed8488
dhs4654 <1934338704@qq.com>
2022-02-05 11:33:27 +0800

    create project structure

--- plform/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-from plform.views import index
-
-urlpatterns = {
-        path("", index, name="index"),
-}
-
--- plform/views.py
+++
@@ -1,9 +0,0 @@
-from django.http import HttpResponse
-
-def index(request):
-    title = '<h1 style = "text-align: center">XMUCITY</h1>'
-    return HttpResponse(title)
-
-
-
-
